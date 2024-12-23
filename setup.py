f18d5252c503a79162b98bfeacaf83b63d999f58
PopeRigby <poperigby@mailbox.org>
2024-12-22 16:24:41 -0800

    Ugh

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author="Cutleast",
     license="CC BY-NC-ND 4.0",
     description="A dynamic patching tool for Skyrim UI mods with strict permissions.",
+    packages=find_packages(),
     include_package_data=True,  # Ensure non-Python files (like .ui and stylesheets) are included
     install_requires=[
         "lz4",
